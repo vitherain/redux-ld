59d990d7873c8a372fcd38eab6e2a67531aaee01
Vít Herain <vit.herain@gmail.com>
2016-10-29 12:42:23 +0200

    excercise default state and Object.assign()

---
+++ 3. Redux & ES6/EX02 - default params, Object.assign()/reducers/images.js
@@ -0,0 +1,10 @@
+function images(state = {data:[], loading:"Please click the 'Random Images' button"}, action){
+    switch (action.type) {
+      case 'IMAGES':
+        return Object.assign({}, state, {data: action.data, loading: "loaded"});
+      case 'IMAGES_LOADING':
+        return Object.assign({}, state, {loading: "loading..."});
+      default:
+        return state;
+    }
+}
