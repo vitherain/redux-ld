20bc1ac0126fa717dd1dd397571d5208ee9f9cbd
Vít Herain <vit.herain@gmail.com>
2016-12-09 19:10:19 +0100

    my refactoring project

---
+++ 5. more examples/1. my refactor/components/Todos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Todos extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <div>
+                <h1>Todo List</h1>
+            </div>
+        )
+    }
+}
