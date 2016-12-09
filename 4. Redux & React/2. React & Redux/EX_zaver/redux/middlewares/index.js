4bc12be3c76ae53083dd0b4ef28a502778294576
Vít Herain <vit.herain@gmail.com>
2016-12-09 16:12:56 +0100

    dalsi cviceni

---
+++ 4. Redux & React/2. React & Redux/EX_zaver/redux/middlewares/index.js
@@ -0,0 +1,12 @@
+//second middleware
+export const crashReporter = store => next => action => {
+    try{
+        next(action);
+    }catch(err){
+        console.group('crashReporter');
+        console.error('error happen with action == ', action);
+        console.error(err);
+        console.groupEnd('crashReporter');
+    }
+
+}
